e4d3555fe0e7d12a012c6de06d2fad0959c7a0aa
nrakochy <nick.rakochy@gmail.com>
2018-01-08 15:23:12 -0600

    Throw RunTime Exception inside bool

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
@@ -377,13 +377,16 @@ class RCTCameraViewFinder extends TextureView implements TextureView.SurfaceText
     }
 
     private boolean cameraAvailable() {
-        if(this._exceptionCounter < 3){
-            this._exceptionCounter ++;
-            throw new RuntimeException("java.lang.NullPointerException: Attempt to invoke virtual method 'android.hardware.Camera$Parameters android.hardware.Camera.getParameters()' on a null object reference");
-        }
         if (_camera != null) {
-            return true;
-        }
+           try {
+            throw new RuntimeException("java.lang.NullPointerException: Attempt to invoke virtual method 'android.hardware.Camera$Parameters android.hardware.Camera.getParameters()' on a null object reference");
+            } catch (Exception e) {
+            throw new RuntimeException("java.lang.NullPointerException: Attempt to invoke virtual method 'android.hardware.Camera$Parameters android.hardware.Camera.getParameters()' on a null object reference");
+            } finally {
+               return true;
+        	}
+	}
+
         return waitForCamera();
     }
 
